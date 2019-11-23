aec8b36e93b199d384366a59d14c0b53782b248c
Thanarat Yangsouy <quntear@gmail.com>
2019-11-24 01:07:52 +0700

    Update Register

--- demo-ec-user/src/main/java/quntear/dec/boundry/Register.java
+++ demo-ec-user/src/main/java/quntear/dec/boundry/Register.java
@@ -1,31 +1,62 @@
 package quntear.dec.boundry;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 @Named
 @RequestScoped
-public class Register {
-
+public class Register implements Serializable {
+	private static final long serialVersionUID = -3024883429735722538L;
+	
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
 	private String confirmPassword;
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getPassword() {
 		return password;
 	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String getConfirmPassword() {
 		return confirmPassword;
+	}
+
+	public void submit() {
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(email);
+		System.out.println(password);
+		System.out.println(confirmPassword);
 	}
 }
