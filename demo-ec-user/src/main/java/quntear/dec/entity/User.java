7ab220184ee8864ce318d4a1512a4df832a038ad
Thanarat Yangsouy <quntear@gmail.com>
2019-11-30 21:28:00 +0700

    added User entity

---
+++ demo-ec-user/src/main/java/quntear/dec/entity/User.java
@@ -0,0 +1,66 @@
+package quntear.dec.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Boolean active;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
