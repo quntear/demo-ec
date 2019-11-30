380e4995dd368c3a68a157fd5080fd4a56a0e401
Thanarat Yangsouy <quntear@gmail.com>
2019-11-30 21:26:28 +0700

    added dependencies and modules

--- demo-ec-user/src/main/java/module-info.java
+++ demo-ec-user/src/main/java/module-info.java
@@ -1,9 +1,16 @@
 module demoec.user {
 	requires jakarta.enterprise.cdi;
+	requires java.annotation;
+	requires java.json.bind;
+	requires java.persistence;
 	requires java.validation;
+	requires java.sql;
+	requires javax.ejb;
 	requires javax.el;
 	requires javax.faces;
 	requires javax.inject;
+	requires javax.security.enterprise.api;
 	requires javax.servlet.api;
+	requires microprofile.config.api;
 	requires omnifaces;
 }
