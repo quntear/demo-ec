9fdc33464aa13a1be5cb71e8ed54fb2bd065a841
Thanarat Yangsouy <quntear@gmail.com>
2019-12-01 18:48:48 +0700

    change http method

--- demo-ec-user/src/main/java/quntear/dec/boundry/UserResource.java
+++ demo-ec-user/src/main/java/quntear/dec/boundry/UserResource.java
@@ -1,7 +1,7 @@
 package quntear.dec.boundry;
 
 import javax.inject.Inject;
-import javax.ws.rs.PUT;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -13,7 +13,7 @@ public class UserResource {
 	@Inject
 	private UserService userService;
 	
-	@PUT
+	@GET
 	@Path("{userId}/actions/activate")
 	public void activateCustomer(@PathParam("userId") Integer userId) {
 		userService.activate(userId);
