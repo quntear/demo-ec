bf7d355f72ec83b8bc796b9ce705734fd477d7fc
Thanarat Yangsouy <quntear@gmail.com>
2019-12-01 17:29:51 +0700

    added create and activate user functions

--- demo-ec-user/src/main/java/quntear/dec/control/UserService.java
+++ demo-ec-user/src/main/java/quntear/dec/control/UserService.java
@@ -1,14 +1,21 @@
 package quntear.dec.control;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+
+import quntear.dec.entity.User;
 
 @Stateless
 public class UserService {
 
 	@PersistenceContext
 	private EntityManager em;
+	
+	@Inject
+	private Pbkdf2PasswordHash passwordHash;
 
 	public boolean exist(String value) {
 		Long count = em.createNamedQuery("User.countByEmail", Long.class).setParameter("email", value)
@@ -16,5 +23,26 @@ public class UserService {
 
 		return count > 0;
 	}
+	
+	public User create(User requestUser) {
+		User entity = new User();
+		entity.setActive(Boolean.FALSE);
+		entity.setEmail(requestUser.getEmail());
+		entity.setFirstName(requestUser.getFirstName());
+		entity.setLastName(requestUser.getLastName());
+		
+		String encodedPassword = passwordHash.generate(requestUser.getPassword().toCharArray());
+		entity.setPassword(encodedPassword);
+		
+		em.persist(entity);
+		
+		return entity;
+	}
 
+	public void activate(Integer userId) {
+		User found = em.find(User.class, userId);
+		
+		found.setActive(Boolean.TRUE);
+		em.merge(found);
+	}
 }
