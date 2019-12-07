585960f9e48083f5b232c219f134052723b26518
Thanarat Yangsouy <quntear@gmail.com>
2019-12-08 00:53:48 +0700

    added UIInput producer

---
+++ demo-ec-seller/src/main/java/quntear/dec/seller/view/QualifierException.java
@@ -0,0 +1,9 @@
+package quntear.dec.seller.view;
+
+public class QualifierException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public QualifierException(String message) {
+		super(message);
+	}
+}
---
+++ demo-ec-seller/src/main/java/quntear/dec/seller/view/UIInputFinder.java
@@ -0,0 +1,71 @@
+package quntear.dec.seller.view;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.visit.VisitContext;
+import javax.faces.component.visit.VisitResult;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+@RequestScoped
+public class UIInputFinder {
+	
+	@Inject
+	private FacesContext facesContext;
+
+	@Produces @Ui
+	public UIInput getInputById(final InjectionPoint ip) {
+		UiQualifier qualifier = getRequireUiQualifierAnnotation(ip);
+		
+		if (Boolean.valueOf(qualifier.postback()) && !facesContext.isPostback()) {
+			return null;
+		}
+		
+		var ids = Arrays.asList(qualifier.id());
+		var found = getInputsList(ids, facesContext.getViewRoot());
+		
+		return found.isEmpty() ? null : found.get(0);
+	}
+	
+	@Produces @Ui
+	public List<UIInput> getInputsById(final InjectionPoint ip) {
+		UiQualifier qualifier = getRequireUiQualifierAnnotation(ip);
+		
+		if (Boolean.valueOf(qualifier.postback()) && !facesContext.isPostback()) {
+			return null;
+		}
+		
+		List<String> ids = Arrays.asList(qualifier.id());
+		return getInputsList(ids, facesContext.getViewRoot());
+	}
+	
+	private UiQualifier getRequireUiQualifierAnnotation(final InjectionPoint ip) {
+		UiQualifier qualifier = ip.getAnnotated().getAnnotation(UiQualifier.class);
+		if (qualifier == null) {
+			throw new QualifierException("required @UiQualifier");
+		}
+		
+		return qualifier;
+	}
+	
+	private List<UIInput> getInputsList(List<String> ids, UIViewRoot root) {
+		List<UIInput> componentCollector = new ArrayList<>();
+		
+		root.visitTree(VisitContext.createVisitContext(facesContext), (visit, target) -> {
+			if (ids.contains(target.getId()) && target instanceof UIInput) {
+				UIInput input = (UIInput) target;
+				componentCollector.add(input);
+			}
+			return VisitResult.ACCEPT;
+		});
+		
+		return componentCollector;
+	}
+}
