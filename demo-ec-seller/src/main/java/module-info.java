b4aa42cdafef7d4159d4882157d9697d1e0bfd80
Thanarat Yangsouy <quntear@gmail.com>
2019-12-08 00:50:08 +0700

    added require modules

--- demo-ec-seller/src/main/java/module-info.java
+++ demo-ec-seller/src/main/java/module-info.java
@@ -1,2 +1,17 @@
 module demoec.seller {
+	requires jakarta.enterprise.cdi;
+	requires java.annotation;
+	requires java.json.bind;
+	requires java.persistence;
+	requires java.validation;
+	requires java.sql;
+	requires java.ws.rs;
+	requires javax.ejb;
+	requires javax.el;
+	requires javax.faces;
+	requires javax.inject;
+	requires javax.security.enterprise.api;
+	requires javax.servlet.api;
+	requires microprofile.config.api;
+	requires omnifaces;
 }
