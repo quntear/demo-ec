60991f935fe0aa306dd50bcbafb2d23bf3a6d214
Thanarat Yangsouy <quntear@gmail.com>
2019-12-15 23:00:34 +0700

    successfully logout redirect to configured login page

--- demo-ec-seller/src/main/java/quntear/dec/seller/boundry/Logout.java
+++ demo-ec-seller/src/main/java/quntear/dec/seller/boundry/Logout.java
@@ -3,8 +3,11 @@ package quntear.dec.seller.boundry;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+
+import quntear.dec.seller.ApplicationConfig;
 
 @Named
 @RequestScoped
@@ -13,8 +16,16 @@ public class Logout {
 	@Inject
 	private HttpServletRequest request;
 	
-	public void submit() throws ServletException {
+	public String submit() throws ServletException {
 		request.logout();
 		request.getSession().invalidate();
+		
+		try {
+			var authMechanismDefinition = ApplicationConfig.class.getAnnotation(CustomFormAuthenticationMechanismDefinition.class);
+			var loginToContinue = authMechanismDefinition.loginToContinue();
+			return loginToContinue.loginPage() + "?faces-redirect=true";
+		} catch (NullPointerException e) {
+			return "login?faces-redirect=true";
+		}
 	}
 }
