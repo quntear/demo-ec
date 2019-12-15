d4b439dc9e371800a65bb858aa32ffefd9397f93
Thanarat Yangsouy <quntear@gmail.com>
2019-12-15 22:57:33 +0700

    protected dashboard path from unauthorized

--- demo-ec-seller/src/main/java/quntear/dec/seller/ApplicationConfig.java
+++ demo-ec-seller/src/main/java/quntear/dec/seller/ApplicationConfig.java
@@ -1,8 +1,14 @@
 package quntear.dec.seller;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
+import javax.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
+import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
 
+@CustomFormAuthenticationMechanismDefinition(loginToContinue = 
+	@LoginToContinue(loginPage = "/login", errorPage = "", useForwardToLogin = false))
 @FacesConfig
+@ApplicationScoped
 public class ApplicationConfig {
 
 }
